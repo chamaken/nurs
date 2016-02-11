897c118811cf77e008675cb4cb7c805fb97e13d8
Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
2016-02-12 06:56:16 +0900

    [nssocket] minor - add license comment and remove unused .h

--- src/nssocket.c
+++ src/nssocket.c
@@ -1,3 +1,15 @@
+/*
+ * (C) 2016 by Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * based on ulogd which was almost entirely written by Harald Welte,
+ * with contributions from fellow hackers such as Pablo Neira Ayuso,
+ * Eric Leblond and Pierre Chifflier.
+ */
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
@@ -14,8 +26,6 @@
 
 #include <linux/limits.h>
 #include <linux/sched.h>
-
-#include <libmnl/libmnl.h>
 
 #include <nurs/nurs.h>
 #include <nurs/list.h>
