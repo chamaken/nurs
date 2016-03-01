263079d8c76d88b831dec4197ff31c22f2b43a1d
Ken-ichirou MATSUZAWA <chamas@h4.dion.ne.jp>
2016-03-01 11:13:35 +0900

    [examples] fix instance specific var holding

--- examples/tick/consumer.go
+++ examples/tick/consumer.go
@@ -15,23 +15,40 @@ package main
 import "C"
 import nurs "../../binding/go"
 
-var myname string
+type tickPriv struct {
+	name string
+}
+
+var privs = make(map[*nurs.Plugin] *tickPriv)
 
 //export tickOrganize
 func tickOrganize(cplugin *C.struct_nurs_plugin) C.enum_nurs_return_t {
 	plugin := (*nurs.Plugin)(cplugin)
 	config := plugin.Config()
-	myname, _ = config.String(0)
+	priv := &tickPriv{}
+	priv.name, _ = config.String(0)
+	privs[plugin] = priv
+
+	return C.enum_nurs_return_t(nurs.RET_OK)
+}
+
+//export tickDisorganize
+func tickDisorganize(cplugin *C.struct_nurs_plugin) C.enum_nurs_return_t {
+	plugin := (*nurs.Plugin)(cplugin)
+	delete(privs, plugin)
 
 	return C.enum_nurs_return_t(nurs.RET_OK)
 }
 
 //export tickInterp
 func tickInterp(cplugin *C.struct_nurs_plugin, cinput *C.struct_nurs_input) C.enum_nurs_return_t {
+	plugin := (*nurs.Plugin)(cplugin)
 	input := (*nurs.Input)(cinput)
+
+	name := privs[plugin].name
 	v, _ := input.U64(0)
 	s, _ := input.String(1)
-	nurs.Log(nurs.INFO, "counter x 1: %d, %s -> %s\n", v, s, myname)
+	nurs.Log(nurs.INFO, "counter x 1: %d, %s -> %s\n", v, s, name)
 	return C.enum_nurs_return_t(nurs.RET_OK)
 }
 
